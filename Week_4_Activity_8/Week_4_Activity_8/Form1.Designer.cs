cff82565caf6dc72a56c5728bc1c0654ffded1e6
Daniel Rumfelt <76234995+GCUStudent-DanielRumfelt@users.noreply.github.com>
2022-07-26 20:49:07 -0400

    Add files via upload

---
+++ Week_4_Activity_8/Week_4_Activity_8/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Week_4_Activity_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.carbohydrateTextBox = new System.Windows.Forms.TextBox();
+            this.fatGramsTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fatCaloriesTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.carbohydrateGramsTextBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(0, 170);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(492, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Fat Grams Consumed:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(704, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Calculate Calories from fat and Carbohydrates";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(0, 83);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(655, 41);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Carbohydrate Grams Consumed:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // carbohydrateTextBox
+            // 
+            this.carbohydrateTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.carbohydrateTextBox.Location = new System.Drawing.Point(12, 127);
+            this.carbohydrateTextBox.Name = "carbohydrateTextBox";
+            this.carbohydrateTextBox.Size = new System.Drawing.Size(188, 22);
+            this.carbohydrateTextBox.TabIndex = 3;
+            // 
+            // fatGramsTextBox
+            // 
+            this.fatGramsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fatGramsTextBox.Location = new System.Drawing.Point(12, 214);
+            this.fatGramsTextBox.Name = "fatGramsTextBox";
+            this.fatGramsTextBox.Size = new System.Drawing.Size(188, 22);
+            this.fatGramsTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(0, 275);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(603, 41);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Fat Calories from Fat Grams for the Day:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // fatCaloriesTextBox
+            // 
+            this.fatCaloriesTextBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.fatCaloriesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fatCaloriesTextBox.Location = new System.Drawing.Point(12, 319);
+            this.fatCaloriesTextBox.Name = "fatCaloriesTextBox";
+            this.fatCaloriesTextBox.Size = new System.Drawing.Size(188, 22);
+            this.fatCaloriesTextBox.TabIndex = 6;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(102, 501);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(113, 49);
+            this.calculateButton.TabIndex = 7;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(326, 501);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(113, 49);
+            this.resetButton.TabIndex = 8;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(542, 501);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(113, 49);
+            this.closeButton.TabIndex = 9;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(5, 376);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(615, 41);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Calories from Carbohydrate for the Day:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // carbohydrateGramsTextBox1
+            // 
+            this.carbohydrateGramsTextBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.carbohydrateGramsTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carbohydrateGramsTextBox1.Location = new System.Drawing.Point(12, 420);
+            this.carbohydrateGramsTextBox1.Name = "carbohydrateGramsTextBox1";
+            this.carbohydrateGramsTextBox1.Size = new System.Drawing.Size(188, 22);
+            this.carbohydrateGramsTextBox1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Maroon;
+            this.label6.Location = new System.Drawing.Point(5, 41);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label6.Size = new System.Drawing.Size(266, 41);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "**Required Field";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Maroon;
+            this.label7.Location = new System.Drawing.Point(206, 129);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(48, 41);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "**";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Maroon;
+            this.label8.Location = new System.Drawing.Point(206, 214);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(48, 41);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "**";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(808, 575);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.carbohydrateGramsTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.fatCaloriesTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fatGramsTextBox);
+            this.Controls.Add(this.carbohydrateTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox carbohydrateTextBox;
+        private System.Windows.Forms.TextBox fatGramsTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fatCaloriesTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox carbohydrateGramsTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Week_4_Activity_8/Week_4_Activity_8/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//A nutritionist who works for a fitness club helps members by evaluating their diets. As part of her evaluation,
+//she asks members for the number of fat grams and carbohydrate grams that they consumed in a day. Then, she calculates
+//the number of calories that result from the fat, using the following formula:
+// Calories from Fat = Fat Grams x 9
+// Next, she calculates the number of calories that result from the carbohydrates using the following formula:
+// Calories from carbs = Carbs grams x 4
+// Create an application that will make these calculations. In the application, you should have the following methods:
+// FatCalories – This method should accept a number of fat grams as an argument and return the number of calories from that amount of fat.
+// CarbCalories – This method should accept a number of carbohydrates grams as an argument and return the number of calories
+// from that amount of carbohydrates.
+
+namespace Week_4_Activity_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Event handler when Calculate Button is pressed.
+        private void calculateButton_Click(object sender, EventArgs e)
+        {   // Create varables for data double.
+            double fatGrams, carbGrams, caloriesFromFat, caloriesFromCarbs;
+
+            // Build a User friendly exception alert.
+            if (double.TryParse(fatGramsTextBox.Text, out fatGrams))
+            {   // Build a User friendly exception alert.
+                if (double.TryParse(carbohydrateTextBox.Text, out carbGrams))
+                {
+                    // Method call for math formula user input * 9 to calculate the amount of calories from fat.
+                    caloriesFromFat = FatCalories(fatGrams);
+                    // Method call for math formula user input * 4 to calculate the amount of calories from Carbs.
+                    caloriesFromCarbs = CarbCalories(carbGrams);
+                    // Send result to GUI in string format to textbox.
+                    fatCaloriesTextBox.Text = caloriesFromFat.ToString();
+                    // Send result to GUI in string format to textbox.
+                    carbohydrateGramsTextBox1.Text = caloriesFromCarbs.ToString();
+                }
+                else
+                {
+                    // Alert box to user to only enter a whole number.
+                    MessageBox.Show("Please enter whole numbers only! ** Fat from Grams Field");
+                }
+            }
+            else
+            {
+                // Alert box to user to only enter a whole number.
+                MessageBox.Show("Please enter whole numbers only! ** Carbohydrate Field");
+            }
+
+        }
+        // Method to calculate fat grams to amount of calories.
+        private double FatCalories(double fGrams)
+        {
+            //Numerical varable to store the value nine.
+            double formula = 9;
+            // User input fat grams * numerical varable  
+            double calories = fGrams * formula;
+            // return answer from calculations
+            return calories;
+        }
+        // Method to calculate Carbs to amount of calories.
+        private double CarbCalories(double cGrams)
+        {
+            //Numerical varable to store the value four.
+            double formula = 4;
+            // User input carbs * numerical varable 
+            double calories = cGrams * formula;
+            // return answer from calculations
+            return calories;
+        }
+        // Rest fields once button is pressed.
+        private void resetButton_Click(object sender, EventArgs e)
+        {   // Clear Field
+            fatGramsTextBox.Text = "";
+            // Clear Field
+            carbohydrateGramsTextBox1.Text = "";
+            // Clear Field
+            carbohydrateTextBox.Text = "";
+            // Clear Field
+            fatCaloriesTextBox.Text = "";
+
+            // Focus Crusor on fatCaloriesTextBox.
+            fatCaloriesTextBox.Focus();
+        }
+        // Close button to end program.
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Terminate Program.
+            this.Close();
+        }
+    }
+}
